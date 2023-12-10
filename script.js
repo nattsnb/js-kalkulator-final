470ef78d32165b37c809e9cab954ef1828bf67a0
Natasza ASzatan <natiye.nb@gmai.com>
2023-12-10 07:44:19 +0000

    added commandNumber to avoid malfunction of the buttons. added error exit when dividing by 0

--- script.js
+++ script.js
@@ -10,48 +10,79 @@ const divideButton = document.querySelector("#divide-button");
 const resultButton = document.querySelector("#result-button");
 const clearButton = document.querySelector("#clear-button");
 
-let inputType = typeof Number(calcInput.value);
+let commandNumber = 0;
 
-if (inputType === "number" && lineOne.innerHTML === "") {
+if (calcInput) {
     plusButton.addEventListener("click", function (){
+        if (commandNumber !==0) {
+            return;
+        }
         lineOne.innerHTML = calcInput.value;
         lineTwo.innerHTML = "+";
         calcInput.value = "";
+        commandNumber = 1;
     })
     minusButton.addEventListener("click", function (){
+        if (commandNumber !==0) {
+            return;
+        }
         lineOne.innerHTML = calcInput.value;
         lineTwo.innerHTML = "-";
         calcInput.value = "";
+        commandNumber = 1;
     })
     multiplyButton.addEventListener("click", function (){
+        if (commandNumber !==0) {
+            return;
+        }
         lineOne.innerHTML = calcInput.value;
         lineTwo.innerHTML = "*";
         calcInput.value = "";
+        commandNumber = 1;
     })
     divideButton.addEventListener("click", function (){
+        if (commandNumber !==0) {
+            return;
+        }
         lineOne.innerHTML = calcInput.value;
         lineTwo.innerHTML = "/";
         calcInput.value = "";
+        commandNumber = 1;
     })
 }
 
-if (inputType === "number") {
+if (calcInput) {
     resultButton.addEventListener("click", function (){
+        if (commandNumber !==1) {
+            return;
+        }
         lineThree.innerHTML = calcInput.value;
         lineFour.innerHTML = "=";
         if (lineTwo.innerHTML === "+") {
             calcInput.value = Number(lineOne.innerHTML) + Number(lineThree.innerHTML);
+            commandNumber = 2;
         }
         if (lineTwo.innerHTML === "-") {
             calcInput.value = Number(lineOne.innerHTML) - Number(lineThree.innerHTML);
+            commandNumber = 2;
         }
         if (lineTwo.innerHTML === "*") {
             calcInput.value = Number(lineOne.innerHTML) * Number(lineThree.innerHTML);
+            commandNumber = 2;
         }
         if (lineTwo.innerHTML === "/") {
+            if (lineThree.innerHTML === "0" || lineThree.innerHTML === "") {
+                lineOne.innerHTML = "";
+                lineTwo.innerHTML = "";
+                lineThree.innerHTML = "ERROR";
+                lineFour.innerHTML = "";
+                calcInput.value = "";
+                commandNumber = 2;
+                return;
+            }
             calcInput.value = Number(lineOne.innerHTML) / Number(lineThree.innerHTML);
+            commandNumber = 2;
         }
-
     })
 }
 
@@ -62,6 +93,7 @@ function clearTheCalc() {
         lineThree.innerHTML = "";
         lineFour.innerHTML = "";
         calcInput.value = "";
+        commandNumber = 0;
     })}
 
 
